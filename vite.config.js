bbf9820ea785c29313dffc2ecceed24e2f641328
Isabella Deng <Isabella.deng2009@gmail.com>
2025-12-06 18:07:04 -0500

    Add GitHub Pages deploy workflow and restore build deps

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  publicDir: 'public',
-  server: {
-    open: true
-  }
-});
-
+  plugins: [],
+  base: '/hacktheridge/', // repo name
+})
